b338f7f1d6ad1d7e7599f4a9fe5b25a2c9abbc8b
Svyatoslav Tup4ienko <89318613+tup4ienko@users.noreply.github.com>
2022-12-30 00:09:59 +0200

    End of section 4

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FirstOrDefaultAsync(
+                    x=> x.Id == request.Id
+                );
+                
+                _context.Remove(activity);
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
